49cadf067ff1f239f904383b98a6ea7ae351ec10
Adarsh Sindagi <avsindagi11@gmail.com>
2026-06-16 11:24:01 +0530

    added frontend

---
+++ binding.js
@@ -0,0 +1,16 @@
+export var color = "red";
+export let name = "nicholas";
+export const magicNumber = 7;
+
+export function sum(num1, num2) {
+    return num1 + num2;
+}
+
+export class Rectangle {
+    constructor(length, width) {
+        this.length = length;
+        this.width = width;
+    }
+}
+
+let inst1=new Rectangle(200,400);
